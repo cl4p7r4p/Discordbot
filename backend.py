56646a69db8cdf3b3c7657542f3becf003da27e8
cl4p-tp <33909900+cl4p7r4p@users.noreply.github.com>
2020-05-24 00:04:46 +0200

    stable release

---
+++ backend.py
@@ -0,0 +1,244 @@
+import aiohttp
+import asyncio
+import json
+import time
+import discord
+import config as cf
+
+def findEventByMsgId(id):
+    for event in raidEvents:
+        if event.messageID == id:
+            return event
+    return -1
+
+
+## RaidEvent Liste initialisieren
+raidEvents = []
+
+protocol = "https"
+server = "cddt-wow.de"
+
+
+base_url = protocol + "://" + server
+
+functions = {
+    "list" : "calevents_list&number=3&raids_only=1", ## **number** = anzahl der nächsten Raids
+    "details": "calevents_details&eventid=",
+    "signup" : "raid_signup&atype=user",
+    "comment" : "add_comment&atype=api",
+    "chars": "user_chars"
+}
+
+## Rollen IDs
+roleDict = {
+    1: "Heal",
+    2: "Tank",
+    3: "Range DD",
+    4: "Melee DD"
+}
+
+## Klassen IDs
+classDict = {
+    2 : "<:druide:673074897869864990>",
+    3 : "<:jger:673074895185248256>",
+    4 : " <:magier:673074898087837717>",
+    6 : "<:priester:673074898519982081>",
+    7 : "<:schurke:673074897450172447>",
+    8 : "<:schamane:673074897806950411>",
+    9 : "<:hexer:673074897790173204>",
+    10 : "<:krieger:673074895386837002>"
+    }
+
+class EmbedEvent():
+    def detailFormat(self):
+        # Function returns the format of signups. 2: roles, 0: classes (used for indexing the tuples)
+        if len(self.data['raidstatus']['status0']['categories']) == 4:
+            self.format = int(2)
+            return
+        elif len(self.data['raidstatus']['status0']['categories']) == 8:
+            self.format = int(0)
+            return
+        else:
+            return -1
+
+    def getClassByID(self,x):
+        # Returniert das Klassen-Emoji
+        if x in classDict:
+            return classDict[x]
+        else:
+            return ""
+
+
+    def getRaidMember(self):
+        ## Charakternamen, Klassen, Rollen und Anmeldestatus aus dem JSON extrahieren
+        for status in self.data['raidstatus']:
+            for category in self.data['raidstatus'][status]['categories']:
+                for char in self.data['raidstatus'][status]['categories'][category]['chars']:
+                    if self.data['raidstatus'][status]['id'] == 0: ## Bestätigte
+                        self.anmeldungen.append((int(self.data['raidstatus'][status]['categories'][category]['chars'][char]['classid']), self.data['raidstatus'][status]['categories'][category]['chars'][char]['name'] + " (B)", int(self.data['raidstatus'][status]['categories'][category]['id'])))
+                    elif self.data['raidstatus'][status]['id'] == 1: ## Anmeldungen
+                        self.anmeldungen.append((int(self.data['raidstatus'][status]['categories'][category]['chars'][char]['classid']), self.data['raidstatus'][status]['categories'][category]['chars'][char]['name'], int(self.data['raidstatus'][status]['categories'][category]['id'])))
+                    elif self.data['raidstatus'][status]['id'] == 2: ## Abmeldungen
+                        self.abmeldungen.append(self.getClassByID(int(self.data['raidstatus'][status]['categories'][category]['chars'][char]['classid'])) + " " + self.data['raidstatus'][status]['categories'][category]['chars'][char]['name'])
+                    elif self.data['raidstatus'][status]['id'] == 3: ## ersatzbank
+                        self.ersatzbank.append(self.getClassByID(int(self.data['raidstatus'][status]['categories'][category]['chars'][char]['classid'])) + " " + self.data['raidstatus'][status]['categories'][category]['chars'][char]['name'])
+                    else:
+                        return -1
+        self.anmeldungen.sort()
+        self.abmeldungen.sort()
+        self.ersatzbank.sort()
+
+    def getListById(self,x):
+        # gibt entweder eine Liste der Rollen oder Klassen aus
+        strList = ""
+        for tup in self.anmeldungen:
+            if tup[self.format] == x:
+                strList += self.getClassByID(tup[0]) + " " + tup[1] + "\n"
+            else:
+                pass
+        # print("Liste:\n" + strList)
+        if len(strList) > 0:
+            return strList
+        else:
+            return ":ghost:"
+
+    def printListToLine(self,liste):
+        if len(liste) > 0:
+            str = (', '.join(liste))
+        else:
+            str = "Niemand"
+        # print("To Line: " + str)
+        return str
+
+    def createEmbed(self):
+        self.getRaidMember()
+        embed = discord.Embed(title=self.raid_title, url=base_url, description="Datum und Zeit: {}".format(self.raid_date), color=0xa73ee6)
+        embed.set_author(name="Die drei R präsentieren:")
+        embed.set_thumbnail(url=self.iconURL)
+        embed.add_field(name="Anmeldungen", value="{} von {} Spielern".format(self.raid_signups,self.raid_maxcount), inline=False)
+        if self.format == 2:
+            embed.add_field(name="Tanks", value=self.getListById(2), inline=True)
+            embed.add_field(name="Heiler", value=self.getListById(1), inline=True)
+            embed.add_field(name="Range DD", value=self.getListById(3), inline=True)
+            embed.add_field(name="Melee DD", value=self.getListById(4), inline=True)
+        elif self.format == 0:
+            embed.add_field(name="Krieger", value=self.getListById(10), inline=True)
+            embed.add_field(name="Jäger", value=self.getListById(3), inline=True)
+            embed.add_field(name="Magier", value=self.getListById(4), inline=True)
+            embed.add_field(name="Priester", value=self.getListById(6), inline=True)
+            embed.add_field(name="Schurke", value=self.getListById(7), inline=True)
+            embed.add_field(name="Schamane", value=self.getListById(8), inline=True)
+            embed.add_field(name="Hexenmeister", value=self.getListById(9), inline=True)
+            embed.add_field(name="Druide", value=self.getListById(2), inline=True)
+
+        embed.add_field(name="Auf der Ersatzbank oder verspätet", value=self.printListToLine(self.ersatzbank), inline=False)
+        embed.add_field(name="Abgemeldet", value=self.printListToLine(self.abmeldungen), inline=False)
+        embed.set_footer(text="Die Raidanmeldung ist bis "+self.deadline+" geöffnet.\nBitte klicke für eine Änderung deines Raidstatus auf die entsprechende Reaktion.")
+
+        self.embedContent = embed
+
+    def __init__(self,id,data):
+        self.id = id
+        self.data = data
+        self.format = 1
+        self.iconURL = base_url + data['icon']
+        self.raid_title = data['title']
+        self.raid_date = data['start']
+        self.deadline = data['deadline']
+        self.deadline_ts = data['deadline_timestamp']
+        self.raid_signups = data['raidstatus']['status0']['count'] + data['raidstatus']['status1']['count']
+        self.raid_maxcount = data['raidstatus']['status0']['maxcount']
+        self.anmeldungen = []
+        self.abmeldungen = []
+        self.ersatzbank = []
+        self.embedContent = None
+        self.detailFormat()
+        self.createEmbed()
+
+
+class RaidEvent():
+    def __init__(self, title, id, starttime, data):
+        self.title = title
+        self.ID = id
+        self.starttime = starttime
+        self.isPosted = False
+        self.messageID =  0
+        self.channelID = 0
+        self.deadline_ts = data['deadline_timestamp']
+        self.creationTime = int(time.time())
+        self.embed = EmbedEvent(id, data)
+        self.iconURL = base_url + data['icon']
+
+    async def update(self):
+        raiddata = await getData(cf.mastertoken, "details", self.ID)
+        if raiddata == self.embed.data:
+            return
+        else:
+            self.embed = EmbedEvent(self.ID, raiddata)
+            self.creationTime = int(time.time())
+
+    async def signup(self, token, memberid, status, note):
+        payload = {
+            "eventid": self.ID,
+            "memberid": memberid,
+            "status": status,
+        }
+        if note != "guffelbot":
+            payload['note'] = note
+        payload = json.dumps(payload)
+        print("trying to post signup")
+        post = await postData(token,"signup", payload)
+        return post
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def getData(token,fun,eventid=0):
+    async with aiohttp.ClientSession() as session:
+        content = await fetch(session, base_url+'/api.php?format=json&atoken='+token+'&function='+functions[fun]+'{}'.format("" if eventid==0 else eventid))
+        # convert response to json
+        content = json.loads(content)
+    return content
+
+
+async def postData(token,fun,payload):
+    # print("trying to post data at: {}".format(base_url+'/api.php?format=json&atoken='+token+'&function='+functions[fun]))
+    # print("with payload: {}".format(payload))
+    try:
+        async with  aiohttp.ClientSession() as session:
+            async with session.post(base_url+'/api.php?format=json&atoken='+token+'&function='+functions[fun], data=payload) as resp:
+                status = await resp.text()
+                print(status)
+        await session.close()
+        return json.loads(status)
+    except:
+        return -1
+
+async def getNextEvents():
+    nextEvents = []
+    data = await getData(cf.mastertoken,"list")
+    for event in data['events']:
+        if data['events'][event]['closed'] == 0:
+            nextEvents.append(data['events'][event]['eventid'])
+        else:
+            pass
+    return nextEvents
+
+async def getRaidDetails(id):
+    eventData = await getData(cf.mastertoken,"details",id)
+    title = eventData['title']
+    start = eventData['start']
+    id = id
+    return title,id,start,eventData
+
+
+async def makeRaidEvents(nextEvents):
+    for eventid in nextEvents:
+        details = await getRaidDetails(eventid)
+        raidEvents.append(RaidEvent(details[0],details[1],details[2],details[3]))
+
+
+async def preperation():
+    eventliste = await getNextEvents()
+    await makeRaidEvents(eventliste)
---
+++ guffelbot.py
@@ -0,0 +1,300 @@
+import aiohttp
+import asyncio
+# import json
+import time
+import discord
+import pickle
+from backend import *
+import config as cf
+
+reactions = ["✅", "🚫", "💤","🔁" ]
+reactDict = {
+    "✅": "anmelden", #status 1
+    "🚫": "abmelden", #status 2
+    "💤": "auf die Bank setzen" #status 3,
+}
+reactStatus = {
+    "✅": 1, #status 1
+    "🚫": 2, #status 2
+    "💤": 3 #status 3
+}
+class Unauthorized(Exception):
+    pass
+
+class Guffelbot(discord.Client):
+    def authorized(self, author):
+        if not author.id in self.registered_users:
+            raise Unauthorized()
+
+
+    def __init__(self, *, loop=None, **options):
+        super().__init__(loop=loop, **options)
+        try:
+            with open('users.pkl', 'rb') as f:
+                self.registered_users = pickle.load(f)
+        except:
+            self.registered_users = {}
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+        try:
+            await preperation()
+            print("Vorbereitung abgeschlossen")
+        except:
+            print("das hat nicht geklappt")
+
+    async def deletemsg(self, message):
+        await message.channel.purge(limit=1)
+
+    def is_me(self,m):
+        return m.author == client.user
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!cddt'):
+            commands = message.content.split(' ')
+            try:
+                await getattr(self, commands[1])(message.author, message.channel, commands[2:])
+            except Unauthorized:
+                auth_embed = discord.Embed(
+                    title="Bitte richte zuerst dein Token ein.",
+                    url="https://cddt-wow.de/index.php/Settings.html?s=",
+                    description="Weitere Hilfe mit `!cddt help setup`"
+                )
+                await message.channel.send(embed=auth_embed)
+            except Exception as e:
+                print(e)
+                await message.channel.send("Fehler beim Bearbeiten Deiner Anfrage")
+
+        if message.content == 'quitt':
+            if message.author.name == "hairypotta":
+                await self.deletemsg(message)
+                await client.close()
+            else:
+                await message.channel.send("Du bist nicht mein Meister :poop:")
+        if message.content == 'clean up':
+            if message.author.name == "hairypotta":
+                await message.channel.purge(limit=50, check=self.is_me)
+            else:
+                await message.channel.send("Du bist nicht mein Meister :poop:")
+
+        if message.content == 'show raids':
+            await self.deletemsg(message)
+            await self.postRaids(message)
+
+    async def postRaids(self,message):
+        for ev in raidEvents:
+            if ev.isPosted and (int(time.time())-ev.creationTime)<120:
+                # await message.channel.send("noch zu jung")
+                pass
+            elif ev.isPosted and (int(time.time())-ev.creationTime)>120:
+                # await message.channel.send("Anzeige wird aktualisiert")
+                msg = await message.channel.fetch_message(ev.messageID)
+                await ev.update()
+                await msg.edit(embed=ev.embed.embedContent)
+
+            else:
+                msg = await message.channel.send(embed=ev.embed.embedContent)
+                ev.isPosted = True
+                ev.messageID = msg.id
+                ev.channelID = msg.channel.id
+                if ev.deadline_ts>int(time.time()):
+                    for emoji in reactions:
+                        await msg.add_reaction(emoji)
+
+    async def on_reaction_add(self, reaction, user):
+        print("reaction von {} registriert".format(user.name))
+        if  reaction.count < 2:
+            return
+        elif reaction.emoji not in reactions:
+            await reaction.remove(user)
+        elif reaction.emoji=="🔁":
+            await reaction.remove(user)
+            await self.postRaids(reaction.message)
+        else:
+            raid = findEventByMsgId(reaction.message.id)
+            try:
+                await reaction.remove(user)
+                await self.signupByReaction(reaction, user, raid)
+            except:
+                await user.send("Du hast leider keine gültige Verbindung zur Raidanmeldung. \nUm das zu ändern, folge den Instruktionen die du von mir mit den Zauberworten:\n **!cddt help setup** \n erhältst.")
+
+
+    async def next(self, author, channel, args):
+        event_embed = discord.Embed(title="Kommende Raids")
+        for raid in raidEvents:
+            event_embed.add_field(
+                name=raid.title,
+                value="{0} von {1} Uhr bis {2} Uhr".format(
+                    '.'.join(reversed(raid.starttime.split(' ')[0].split('-'))),
+                    raid.starttime.split(' ')[1],
+                    raid.starttime.split(' ')[1],
+                ),
+                inline=False)
+        await channel.send(embed=event_embed)
+
+    async def help(self, author, channel, args):
+        if args == [] or args == None:
+            await channel.send("""
+```asciidoc
+Verfügbare Befehle:
+----------------------
+[Tokeneinrichtung]
+- !cddt setup <TOKEN>
+
+[Kommende Raids]
+- !cddt next
+
+[Raidstatus]
+- !cddt status
+
+Mehr Hilfe zu den Befehlen mit: !cddt help <BEFEHL>
+```
+            """)
+        else:
+            try:
+                doc = getattr(self, args[0]).__doc__
+                await channel.send(doc)
+            except Exception as e:
+                print(e)
+                await channel.send("Dafür fehlt mir leider der Hilfetext, informiere Theo")
+
+    async def setup(self, author, channel, args):
+        """__**Einrichtung des EQDKP Tokens**__
+    Um die Funktionen des Discord Bots zu nutzen musst du auf der Webseite \
+https://cddt-wow.de registriert und freigeschaltet sein.
+
+    Navigiere zu den `Registrierungs-Details`, indem du auf der Webseite oben links auf deinen \
+Benutzernamen klickst und dann dem Link zu `Einstellungen` folgst. Alternativ nutze diesen Link \
+    https://cddt-wow.de/index.php/Settings.html?s=.
+
+    Dein Token findest du innerhalb der `Registrierungs-Details` unter `Private Schlüssel` \
+als `Privater API-Schlüssel`. Du kannst rechts auf `**********` klicken um es dir \
+anzeigen zu lassen. Kopiere es um dann den `setup`-Befehl mit deinem Token auszuführen.
+
+`!cddt setup 12345ab34dc...34255612313`
+
+    Benutze den `setup`-Befehl nur in Direktnachrichten mit dem Bot. Sonst hat jeder dein Token \
+und kann sich unter deinem Namen für Raids anmelden. Sollte dein Token einmal in fremde Hände \
+gelangen, kannst du auf der Webseite, dort wo du auch dein Token gefunden hast, neue Schlüssel generieren \
+und den `setup`-Befehl erneut ausführen.
+        """
+
+        if not isinstance(channel, discord.DMChannel):
+            await channel.send("Bitte mach das in einem privaten Chat mit mir!")
+            return
+        if len(args) < 1:
+            await channel.send("Da war leider kein Token dabei.")
+            return
+        if author.id in self.registered_users:
+            await channel.send("Dein Token wird aktualisiert.")
+        else:
+            await channel.send("Dein Token wird hinzugefügt.")
+        self.registered_users[author.id] = args[0]
+        try:
+            with open('users.pkl', 'wb') as f:
+                pickle.dump(self.registered_users, f)
+        except Exception as e:
+            print(e)
+            await channel.send("Permanente Nutzerregistrierung fehlgeschlagen, bitte informiere Theo.")
+
+    async def selection_helper(self, prompt, list, author, channel):
+        def check(message):
+            return message.author == author and message.channel == channel
+
+        select_embed = discord.Embed(title=prompt)
+        select_embed.set_footer(text="Bitte mit der Nummer deiner Auswahl antworten.")
+        for i in range(len(list)):
+            select_embed.add_field(name=str(i+1), value=list[i])
+        await channel.send(embed=select_embed)
+        msg = await self.wait_for('message', check=check)
+        if int(msg.content)-1 in range(len(list)):
+            return int(msg.content)
+        else:
+            raise ValueError('Out of range')
+
+    async def note_helper(self, author, channel):
+        def check(message):
+            return message.author == author and message.channel == channel
+
+        await channel.send("Wie lautet deine Notiz?")
+        msg = await self.wait_for('message', check=check)
+        if len(msg.content)>0:
+            return str(msg.content)
+        else:
+            print('Fehler in der Notizabfrage: Notiz zu kurz')
+
+    async def signupByReaction(self, reaction, user, raidevent):
+        print(user.id)
+
+        self.authorized(user)
+        status_options = [":sparkle: Bestätigt", ":white_check_mark: Angemeldet", ":no_entry_sign: Abgemeldet", ":zzz: Ersatzbank"]
+
+        msg = await user.send("Hey {}.".format(user.name))
+        try:
+            answer = await self.selection_helper("Du willst Dich für den Raid __**{}**__ **{}**?".format(raidevent.title,reactDict[reaction.emoji]), ["Ja", "Nein"], user, msg.channel)
+            if answer == 1:
+                try:
+                    char_options = []
+                    char_ids = []
+                    note = "powered by guffelbot"
+                    chars = await getData(self.registered_users[user.id],"chars")
+                    for char in chars['chars']:
+                        print(char)
+                        char_options.append(chars['chars'][char]['name'])
+                        char_ids.append(chars['chars'][char]['id'])
+                    if len(char_options) > 1:
+                        charidx = await self.selection_helper("Welcher Charakter?", char_options, user, msg.channel)-1
+                    else:
+                        charidx = 0
+                    notiz = await self.selection_helper("Möchtest du deiner Anmeldung eine Notiz hinzufügen?", ["Ja", "Nein"], user, msg.channel)
+                    print("antwort auf notizfrage: {}".format(notiz))
+                    if notiz == 1:
+                        note = await self.note_helper(user, msg.channel)
+                        print("eingegebene notiz: {}".format(note))
+
+                except Exception as e:
+                    print(e)
+                    await msg.channel.send("Eine seltsame Auswahl, ich breche den Vorgang ab.")
+                    return
+            else:
+                await msg.channel.send("Abgebrochen")
+                return
+        except Exception as e:
+            print(e)
+            await msg.channel.send("Eine seltsame Auswahl, ich breche den Vorgang ab.")
+            return
+        await msg.channel.send("Dein Anmeldestatus wird aktualisiert.")
+        r =  await raidevent.signup(self.registered_users[user.id], char_ids[charidx], reactStatus[reaction.emoji],note)
+        print("response: {}".format(r))
+        print("type: {}".format(type(r)))
+        try:
+            if r['status'] == 1:
+                success_embed = discord.Embed(
+                    title="Alles klar!",
+                    description="Dein Status für **{}** am {} wurde aktualisiert.\n **Status:** {}\n**Notiz:** {}".format(raidevent.title,raidevent.starttime,reaction.emoji,note)
+                )
+                success_embed.set_thumbnail(url=raidevent.iconURL)
+                await msg.channel.send(embed=success_embed)
+            else:
+                if r['error'] == 'required data missing' and r['info'] == 'roleid':
+                    await msg.channel.send(embed=discord.Embed(
+                        title="Standardrolle setzen!",
+                        url="https://cddt-wow.de/index.php/MyCharacters/?s=",
+                        description="Du hast keine Standardrolle für deinen gewählten Charakter gesetzt. Bitte klicke oben auf den Link um dies nachzuholen."
+                    ))
+                    return
+                print(r)
+                await msg.channel.send("Das hat nicht geklappt.")
+        except Exception as inst:
+            print(type(inst))    # the exception instance
+            print(inst.args)     # arguments stored in .args
+            print(inst)
+
+
+client = Guffelbot()
+
+client.run(cf.guffeltoken1) #1: test #2: live
